90f36bd72fe203b9df2e106e2fb3bc6abe9a25f7
Asif Shariar <shariarasif0@gmail.com>
2025-10-29 10:01:48 +0600

    Delete Simple Guessing Game directory

--- Simple Guessing Game/script.js
+++
@@ -1,9 +0,0 @@
-let gameInput = 29;
-let userInput = prompt("Guess any number between (1-30): ");
-
-while (gameInput != userInput) {
-
-    userInput = prompt("Wrong Guess. Guess again: ");
-}
-
-document.write("Congratulation, your guess is correct!");
